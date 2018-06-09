572cabe3f71db08c7cf11780179ce7e083ce7a01
deathangel908 <deathangel908@gmail.com>
2018-06-10 02:11:09 +0300

    MiniCssExtractPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 const path = require('path');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   entry: ['./src/main.js'],
   plugins: [
     new VueLoaderPlugin(), // required since webpack 4 and v14,
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
   ],
   resolve: {
     alias: {
@@ -19,7 +26,15 @@ module.exports = {
         loader: 'vue-loader', //allows singleFileComponents
 
       },
-      {test:  /\.(sass|scss)$/, loader: 'style-loader!css-loader!sass-loader'},
+      {
+        test: /\.sass$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "style-loader",
+          "css-loader",
+          "sass-loader?indentedSyntax"
+        ]
+      },
     ]
   }
 
