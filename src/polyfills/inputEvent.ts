f50b1033b924bbe6f49cbe0144190db4b05d3d4d
Andrew Koidan <deathangel908@gmail.com>
2020-01-14 15:01:17 +0200

    Fix

--- src/polyfills/inputEvent.ts
+++ src/polyfills/inputEvent.ts
@@ -1,8 +1,9 @@
 /**
  *  Hotfix for Edge 15 for reflect data
  */
+// eslint-disable
 // istanbul ignore next
-if (!window.InputEvent) { // eslint-disable-line
+if (!window.InputEvent) {
   // @ts-ignore: next-line
   window.InputEvent = (): void => {};
 }
